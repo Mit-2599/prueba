9f6b7583701ce1d8cbebcedc4d5d7b16a65c33cf
Mit-2599 <mitpatel2599@gmail.com>
2020-06-26 17:59:02 -0500

    Lab3Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { StudioComponent } from './components/studio/studio.component';
+import { PeopleComponent } from './components/people/people.component';
+import { TalkComponent } from './components/talk/talk.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StudioComponent,
+    PeopleComponent,
+    TalkComponent
   ],
   imports: [
     BrowserModule,
